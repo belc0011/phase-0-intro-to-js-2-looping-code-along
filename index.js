4968780b949c10844f60ab04c794ee4b265e948c
Monica Garcia <monicairenegarcia@gmail.com>
2023-11-04 10:55:55 -0700

    Completed looping lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+const newArray = []
+function writeCards(array, event) {
+    for (let i = 0; i < array.length; i++) {
+        newArray[i] = `Thank you, ${array[i]}, for the wonderful ${event} gift!`
+    }
+    return newArray;
+}
+
+function countDown(int) {
+    while (int > -1) {
+        console.log(int--);
+    }
+}
+
+
